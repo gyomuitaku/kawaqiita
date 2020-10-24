cd37bb984150572dcd34fdbe5e64bbe28e15eaaf
sen-com <nogenon@docomo.ne.jp>
2020-10-24 17:34:35 +0900

    add toeic2000

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -2,5 +2,8 @@ class TopsController < ApplicationController
   def top
     file = open("./word/word_test.txt")
     @lines = file.read().split("\n")
+    require 'roo'
+    @xlsx = Roo::OpenOffice.new('./word/toeic2000.ods')
+    @xlsx.default_sheet = @xlsx.sheets[0]
   end
 end
