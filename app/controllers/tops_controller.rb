cfdfed78fac03a84d684996ab701d4fa8c6666dc
ishishow <tumekiri32515049@outlook.jp>
2020-10-25 11:37:47 +0900

    modify logic quiz

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,6 +1,5 @@
 class TopsController < ApplicationController
   def top
-    require 'roo'
     @xlsx = Roo::OpenOffice.new('./word/toeic2000.ods')
     @xlsx.default_sheet = @xlsx.sheets[0]
     @lines = Fileloader.get_lines
--- app/libs/Quiz.rb
+++ app/libs/Quiz.rb
@@ -7,10 +7,22 @@ class Quiz
     end
 
     def self.generate
-        questions = []   
-        Fileloader.get_lines.each do |line|
-        item = line.chomp.split("/", 2) 
-        questions.push(Quiz.new(item[0],[item[1], "a", "b", "c"],item[1]))
+        questions, i = [], 0,
+        lines = Fileloader.get_lines 
+
+        while i < 3 do
+            choice_array = Array.new(3){ rand 98 }
+            item = []
+            item.push(lines[i].chomp.split("/", 2))
+            for j in choice_array do
+                if j != i
+                    item.push(lines[j].chomp.split("/", 2))
+                else
+                    item.push(lines[j+1].chomp.split("/", 2))
+                end
+            end
+            questions.push(Quiz.new(item[0][0],[item[0][1], item[1][1], item[2][1], item[3][1]].shuffle,item[0][1]))
+            i += 1
         end
         questions
     end
