0d5f4f32ad42c12470e22bd99f5e75847cfcb7bc
ishishow <tumekiri32515049@outlook.jp>
2020-10-27 00:58:27 +0900

    コントローラにデータを送った

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -14,4 +14,18 @@ class TopsController < ApplicationController
     gon.questions = Quiz.generate
   end
 
+  def input
+    byebug
+    quizResults = params[:quizResults]
+    i = 0
+    while (quizResults[i.to_s] != nil) do
+      result = Result.new()
+      result.word = quizResults[i.to_s][:en_word]
+      result.meaning = quizResults[i.to_s][:answer]
+      result.is_correct = quizResults[i.to_s][:status]
+      result.user_id = 0
+      result.save
+      i += 1
+    end
+  end
 end
