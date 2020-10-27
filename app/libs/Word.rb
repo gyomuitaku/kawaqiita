dc1d58c48769478206878ad9cc751351808e6238
vagrant <vagrant@localhost.localdomain>
2020-10-27 08:32:22 +0000

    add table's row

--- app/libs/Word.rb
+++ app/libs/Word.rb
@@ -8,9 +8,9 @@ class Word
     def self.generate
     	words, i = [], 0,
         xlsx = Fileloader.get_lines
-        xlsx_choice = (2..2001).to_a.sample(50)
+        xlsx_choice = (2..2001).to_a.sample(52)
 
-        for row in 1..50 do
+        for row in 1..51 do
             item = []
             item.push(xlsx.row(xlsx_choice[row]))
             words.push(Word.new(item[0][1],item[0][2]))
