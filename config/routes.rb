d7c9b334c932d943a7ff57b001507a7b6a65c2f0
ishishow <tumekiri32515049@outlook.jp>
2020-10-27 23:06:02 +0900

    データベースリファクタリング

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => 'tops#home'
   get "top" => "tops#top"
   get "quiz" => "tops#quiz"
-  post "quiz" => "tops#input"
   get "kakutan" => "tops#kakutan"
   get "select" => "tops#select"
+  post 'results' => "results#create"
 end
