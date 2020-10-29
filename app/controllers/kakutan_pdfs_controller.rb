092cdf9270563e4cda7a1f1986d93d3fdb0c3bed
vagrant <vagrant@localhost.localdomain>
2020-10-29 17:31:37 +0000

    almost complete

--- app/controllers/kakutan_pdfs_controller.rb
+++ app/controllers/kakutan_pdfs_controller.rb
@@ -1,12 +1,19 @@
 class KakutanPdfsController < ApplicationController
   def index
-  	@results = Result.all # pdf上で使用するレコードのインスタンスを作成
+    data = []
+    for i in 0..74 do
+      data.push([params[:word_set][:words][i][:english_first], params[:word_set][:words][i][:japanese_first], params[:word_set][:words][i][:english_second], params[:word_set][:words][i][:japanese_second]])
+    end
+
+    @results = data
+    @page = params[:page].to_i
+    # binding.pry
     respond_to do |format|
       format.html
       format.pdf do
 
         # pdfを新規作成。インスタンスを渡す。
-        pdf = KakutanPdf.new(@results)
+        pdf = KakutanPdf.new(@results, @page)
 
         send_data pdf.render,
           filename:    "sample.pdf",
--- app/pdfs/kakutan_pdf.rb
+++ app/pdfs/kakutan_pdf.rb
@@ -1,7 +1,7 @@
 class KakutanPdf < Prawn::Document
 
 	# recordにモデルなどのデータを渡します
-	def initialize(result)
+	def initialize(result, page)
 
 	  # superで初期設定を指定します(ページサイズ、マージン等)
 	  super(
@@ -16,6 +16,7 @@ class KakutanPdf < Prawn::Document
 	  # stroke_axis
 
 	  @result = result # インスタンスを受け取り。コンポーネント作成時などにレコード内のデータを使える
+	  @page = page
 
 	  header
 	  contents
@@ -29,12 +30,20 @@ class KakutanPdf < Prawn::Document
 	def contents
 		data = []
 		# binding.pry
-		for i in 1..25 do
-			data.push([@result.find(i).word, @result.find(i).meaning, @result.find(i+25).word, @result.find(i+25).meaning])
+		min = 0
+		max = 24
+		for i in 1..@page do
+			for j in min..max do
+				data.push([@result[j][0], @result[j][1], @result[j][2], @result[j][3]])
+			end
+			for j in min..max do
+				data.push([@result[j][0], "", @result[j][2], ""])
+			end
+			min += 25
+			max += 25
 		end
-		data_shuffle = data.shuffle
-		data_shuffle.unshift(["英語", "日本語", "英語", "日本語"])
-		table data_shuffle, cell_style: { size: 10, height: 28, width: 120 , padding_top: 8, padding_left: 7 }, header: true do |t|
+		data.unshift(["英語", "日本語", "英語", "日本語"])
+		table data, header: true, cell_style: { size: 10, height: 28, width: 120 , padding_top: 8, padding_left: 7 }, header: true do |t|
 	      t.before_rendering_page do |page|
 	        page.row(0).border_bottom_width = 3
 	      end
