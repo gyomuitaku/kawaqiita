de72e37d943cb991aae296be005f6f0b5ec174b8
gyoumuitaku <yo0314baseball@gmail.com>
2020-10-29 12:24:59 +0000

    type修正

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -2,4 +2,22 @@ class ResultsController < ApplicationController
   def create
     Result.save_results(params[:quizResults])
   end
+  def create_type
+    if params[:results].present?
+      result =Result.new
+      result.word =params[:results][:answer]
+      result.meaning =params[:results][:text]
+      result.is_correct =1
+      result.user_id =1
+      result.save
+    end
+    if params[:miss].present?
+      result =Result.new
+      result.word =params[:miss][:answer]
+      result.meaning =params[:miss][:text]
+      result.is_correct =0
+      result.user_id =1
+      result.save
+    end
+  end
 end
