f31d16fa15268ee8734e8f576369787e78044ce5
sen-com <nogenon@docomo.ne.jp>
2020-10-30 00:39:50 +0900

    select file

--- app/libs/Fileloader.rb
+++ app/libs/Fileloader.rb
@@ -1,8 +1,16 @@
 class Fileloader
 
-    def self.get_lines
-        xlsx = Roo::OpenOffice.new('./word/toeic2000.ods')
-        xlsx.default_sheet = xlsx.sheets[0]
+    def self.get_lines(word_file)
+        if word_file == 0
+            xlsx = Roo::OpenOffice.new('./word/toeic2000.ods')
+            xlsx.default_sheet = xlsx.sheets[0]
+        elsif word_file == 1
+            xlsx = Roo::OpenOffice.new('./word/deep.ods')
+            xlsx.default_sheet = xlsx.sheets[0]
+        elsif word_file == 2
+            xlsx = Roo::OpenOffice.new('./word/novel.ods')
+            xlsx.default_sheet = xlsx.sheets[0]
+        end
         return xlsx
     end
 
