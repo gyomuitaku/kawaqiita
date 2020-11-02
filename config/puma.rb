7dc87176659a96c86434fcd709abe6efc28e7517
ishishow <ishikawa.sho.infratop@gmail.com>
2020-11-02 17:45:13 +0900

    I Want resolve

--- config/puma.rb
+++ config/puma.rb
@@ -35,3 +35,8 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+
+# to resolve 502 Gateway
+bind "unix:///var/run/puma/my_app.sock" 
+pidfile "/var/run/puma/my_app.sock" 
