7d92c93849cb0f4a27825fa896634b5f0d3d789d
sho.ishikawa <sho.ishikawa@venect.jp>
2020-11-02 14:24:33 +0900

    add assets styles js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+// = require jquery3
+//= require popper
+//= require bootstrap-sprockets
+//= require jquery
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
