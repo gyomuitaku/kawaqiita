d6c48e4ef90f01b2d0043855433df5f1ec9867b1
ishishow <ishikawa.sho.infratop@gmail.com>
2020-11-02 14:17:14 +0900

    change route and add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  resources :tmps
-  get 'welcome_page/welcome'   
-  root 'welcome_page#welcome'
+  post 'kakutan_pdfs/index'
+  devise_for :users
+  resources :users
+  root :to => 'tops#home'
+  get "top" => "tops#top"
+  get "quiz" => "tops#quiz"
+  get "type" => "tops#type"
+  get "kakutan" => "tops#kakutan"
+  get "select" => "tops#select"
+  post 'results' => "results#create"
+  post "kakutan" => "tops#kakutan"
+  post 'type/results' => "results#create_type"
+  post 'word/json' => "word_sets#create"
 end
